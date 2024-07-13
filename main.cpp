10a31baa6aacf019b4c3cda8e3d8af1f3be8179d
Neiland <baramakc@inbox.ru>
2024-07-13 12:13:52 +0300

    Сделан минимальный интерфейс в main

--- main.cpp
+++ main.cpp
@@ -2,7 +2,12 @@
 #include "register.h"
 
 int main() {
-	login();
+	std::cout << "What you would have: login or registration?\n";
+	std::string buffer;
+	std::cout << "Input: "; std::cin >> buffer;
+	if (buffer == "login") login();
+	else if (buffer == "registration") registration();
+	else std::cout << "Your command don't exists!\n";
 
 
 }
